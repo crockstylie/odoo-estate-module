6024cd36780171190959eaea1c37aaabcb819f7e
crockstylie <antoine.hory@gmail.com>
2021-11-07 18:17:46 +0100

    adding an inverted value : deadline

--- models/estate_property_offer.py
+++ models/estate_property_offer.py
@@ -1,4 +1,4 @@
-import datetime
+from dateutil.relativedelta import relativedelta
 
 from odoo import fields, models, api
 
@@ -7,9 +7,21 @@ class EstatePropertyOffer(models.Model):
     _name = "estate.property.offer"
     _description = "Estate property offer"
 
-    price = fields.Float(string='Price')
-    status = fields.Selection(
-        [
+    price = fields.Float(
+        string='Price'
+    )
+    validity = fields.Integer(
+        string="Validity (days)",
+        default=7
+    )
+    date_deadline = fields.Date(
+        string="Deadline",
+        compute="_compute_date_deadline",
+        inverse="_inverse_date_deadline"
+    )
+    state = fields.Selection(
+        string="Status",
+        selection=[
             ('accepted', 'Accepted'),
             ('refused', 'Refused')
         ],
@@ -23,3 +35,14 @@ class EstatePropertyOffer(models.Model):
         'estate.property',
         string="Property ID"
     )
+
+    @api.depends("create_date", "validity")
+    def _compute_date_deadline(self):
+        for offer in self:
+            date = offer.create_date.date() if offer.create_date else fields.Date.today()
+            offer.date_deadline = date + relativedelta(days=offer.validity)
+
+    def _inverse_date_deadline(self):
+        for offer in self:
+            date = offer.create_date.date() if offer.create_date else fields.Date.today()
+            offer.validity = (offer.date_deadline - date).days
